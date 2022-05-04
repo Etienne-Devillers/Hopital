d3f2b0c6360ce3f455224bddd33bee0b5f489f77
Etienne-Devillers <devillers.etienne80@gmail.com>
2022-05-04 11:41:09 +0200

    singleton

--- config/db.php
+++ config/db.php
@@ -9,16 +9,23 @@ define('PWD', '_!CY1P-yR_q)1eg]');
 
 class Database {
 
-
+    private static $pdo;
+    
     public static function dbConnect():object{
     
             try{
-                $pdo = new PDO(DSN, USERDB, PWD);
-                $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ); 
+                if (is_null(self::$pdo)){
+                    self::$pdo = new PDO(DSN, USERDB, PWD);
+                    self::$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+                }
+                
             }
             catch(PDOException $e){
                 header('location: /controllers/error-controller.php?id=1');
             }
-            return $pdo;
+
+            return self::$pdo;
     }
+
+
 }
